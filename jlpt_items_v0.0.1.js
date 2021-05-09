8fc723392b25043c1933ae8d2bde892de11a15a2
Amir DIB <dib.amir@gmail.com>
2021-05-09 10:33:48 +0200

    Create jlpt_items_v0.0.1.js

---
+++ jlpt_items_v0.0.1.js
@@ -0,0 +1,51 @@
+//===================================================================
+// Kanji list
+//-------------------------------------------------------------------
+window.jlpt_items.kanji = {
+	N5: [// [characters, meanings, kunyomi, onyomi]
+		['人', 'person', 'ひと,り,と', 'じん,にん'],
+		['日', 'day,sun,Japan,counter for days', 'ひ,び,か', 'にち,じつ'],
+	],
+	N4: [// [characters, meanings, kunyomi, onyomi]
+		['同', 'same,agree,equal', 'おな', 'どう'],
+		['会', 'meeting,meet,party,association,interview,join', 'あ,あつ', 'かい,え'],
+	],
+	N3: [// [characters, meanings, kunyomi, onyomi]
+		['内', 'inside,within,between,among,house,home', 'うち', 'ない,だい'],
+		['政', 'politics,government', 'まつりごと,まん', 'せい,しょう'],
+	],
+	N2: [// [characters, meanings, kunyomi, onyomi]
+		['村', 'village,town', 'むら', 'そん'],
+		['党', 'party,faction,clique', 'なかま', 'とう'],
+	],
+	N1: [// [characters, meanings, kunyomi, onyomi]
+		['氏', 'family name,surname,clan', 'うじ', 'し'],
+		['統', 'overall,relationship,ruling,governing', 'す,ほび', 'とう'],
+	]
+};
+
+//===================================================================
+// Vocabulary list
+//-------------------------------------------------------------------
+window.jlpt_items.vocabulary = {
+	N5: [// [characters, meanings, readings]
+		['学校', 'school', 'がっこう'],
+		['川', 'river,stream', 'かわ'],
+	],
+	N4: [// [characters, meanings, readings]
+		['腕', 'arm', 'うで'],
+		['人形', 'doll,puppet,figure', 'にんぎょう'],
+	],
+	N3: [// [characters, meanings, readings]
+		['正直', 'honest,frank,candid,straightforward', 'しょうじき'],
+		['博物館', 'museum', 'はくぶつかん'],
+	],
+	N2: [// [characters, meanings, readings]
+		['儀式', 'ceremony,rite,ritual,service', 'ぎしき'],
+		['職人', 'craftsman,artisan,tradesman,worker,workman', 'しょくにん'],
+	],
+	N1: [// [characters, meanings, readings]
+		['切り', 'end,finish,stop', 'きり'],
+		['農場', 'farm', 'のうじょう'],
+	]
+};
